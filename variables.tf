793a4a2710e5bfb59d5384c33318c87e9ad447e2
zabarishi <46435964+zabarishi@users.noreply.github.com>
2023-02-08 15:33:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
-variable "resource_group_name" {
+variable "resource_group_name01" {
   description = "The name of resource group "
 }
-
+variable "resource_group_name02" {
+  description = "The name of resource group "
+}
 variable "location" { 
   description = "Location "
 }
