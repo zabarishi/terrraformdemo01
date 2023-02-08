af76c17076fe46e7e998b1515596eb144eb3fbc5
zabarishi <46435964+zabarishi@users.noreply.github.com>
2023-02-08 15:32:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resourceGroupName
   location = var.location
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resourceGroupName
+  location = var.location
+}
